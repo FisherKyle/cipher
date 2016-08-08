95448e244b1358ed7b36abcebf2219bae9e693f0
Alexandre Leibler and Kyle Fisher <alexandre+kyle@>
2016-08-08 17:05:54 -0400

    created js to reverse string

--- js/calchomework.js
+++ js/calchomework.js
@@ -8,3 +8,31 @@ $(document).ready(function() {
     });
 
 });
+
+var getSentence = function() {
+  var sentence = prompt("Input sentence");
+  return sentence;
+};
+
+var getCapitals = function(myString) {
+  var myStringLength = myString.length;
+  var capitalFirst = myString.charAt(0).toUpperCase();
+  var capitalLast = myString.charAt(myStringLength - 1).toUpperCase();
+  alert("Capitals: " + capitalFirst + " " + capitalLast);
+  return (capitalFirst + capitalLast);
+}
+
+
+
+var reverseCapitals = function(reversedString) {
+  var reversedStringLength = reversedString.length;
+  var temp = "";
+
+  for (var i = reversedStringLength; i >= 0 ; i--) {
+    temp = temp + reversedString.charAt(i);
+  }
+  alert("reversedString: " + temp);
+  return temp;
+}
+
+reverseCapitals(getCapitals(getSentence()));
