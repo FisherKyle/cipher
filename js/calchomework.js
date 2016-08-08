e8b8d7900f3614f4e6f9f45e1d2a04a23964f012
Alexandre Leibler and Kyle Fisher <alexandre+kyle@>
2016-08-08 16:19:07 -0400

    link images and make them clickable by jQuery

---
+++ js/calchomework.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".geometric img").click(function() {
+      alert("This is where the ");
+    });
+
+    $(".abstract img").click(function() {
+      alert("This is where the ");
+    });
+
+});
